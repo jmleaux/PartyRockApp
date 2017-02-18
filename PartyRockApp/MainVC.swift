362b744bb064a3095126356e214be50b309166fb
jmleaux <chip@jmlowe.net>
2017-02-18 12:32:39 +0300

    initial layout in progress

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by J. M. Lowe on 2/18/17.
+//  Copyright © 2017 JMLeaux LLC. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+
+}
+
